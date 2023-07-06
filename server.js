71fd0b5471c26516705d6584296e22c987d7fec8
layersony <sammaingi5@gmail.com>
2023-07-06 09:32:08 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3200;
+
+server.use(middlewares);
+server.use(router);
+
+console.log(`Starting server.....`)
+server.listen(port);
+console.log(`Server running in port ${port}`)
